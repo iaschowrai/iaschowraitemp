736f33e30bfd030cfcfef0e60c133ea807fc3378
iaschowrai <iaschowrai@gmail.com>
2024-08-24 20:00:17 -0400

    designation size changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
                     variants={container(0.5)}
                     initial="hidden"
                     animate="visibility"
-                    className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                    className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
                         Full Stack Developer
                     </motion.span>
                     <motion.p 
