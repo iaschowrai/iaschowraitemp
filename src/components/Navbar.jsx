38f8b713430704321ad3c4d3e8c3c33627bd0c82
Irshad Ahmed <88178320+iaschowrai@users.noreply.github.com>
2024-09-18 14:10:54 -0400

    fetching resume from external source

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,53 @@
-import logo from "../assets/images/IAlogo.png"
-import { FaDownload } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import  resume  from "../assets/resume/Resume_fullstack_IrshadAhmed.pdf"
+import logo from "../assets/images/IAlogo.png";
+import { FaDownload, FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
+  // Google Drive direct download link
+  const resumeLink = "https://drive.google.com/uc?export=download&id=11RLU4pwwndmhGiUaUK4Z6OB1rOiV7zni";
+
   return (
     <nav className="mb-20 flex item-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-            <a href="https://www.iaschowrai.com"><img className="mx-2 w-10" src={logo} alt="logo" /></a>
-        </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <span className="flex flex-wrap gap-2 font-thin tracing-tight text-sm">Download Resume -&gt;</span>
-            <a href={resume}
-              // download
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Download Resume"
-                title="Download Resume"
-                className="text-cyan-400 hover:text-cyan-600 transition-colors">
-              <FaDownload />
-            </a>
-            <a href="https://www.linkedin.com/in/irshadahmeds/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="LinkedIn Profile"
-                title="LinkedIn Profile"
-                // className="text-cyan-400 hover:text-cyan-600 transition-colors"
-                >
-            <FaLinkedin />
-            </a>
-            <a href="https://github.com/iaschowrai"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Github Profile"
-                title="Github Profile"
-                // className="text-cyan-400 hover:text-cyan-600 transition-colors"
-                >
-              <FaGithub />
-            </a>
-        </div>
+      <div className="flex flex-shrink-0 items-center">
+        <a href="https://www.iaschowrai.com">
+          <img className="mx-2 w-10" src={logo} alt="logo" />
+        </a>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <span className="flex flex-wrap gap-2 font-thin tracking-tight text-sm">
+          Download Resume -&gt;
+        </span>
+        <a
+          href={resumeLink}
+          download
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Download Resume"
+          title="Download Resume"
+          className="text-cyan-400 hover:text-cyan-600 transition-colors"
+        >
+          <FaDownload />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/irshadahmeds/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn Profile"
+          title="LinkedIn Profile"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/iaschowrai"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Github Profile"
+          title="Github Profile"
+        >
+          <FaGithub />
+        </a>
+      </div>
     </nav>
-  )
+  );
 };
 
-export default Navbar
+export default Navbar;
